2f6d6772de1bc7129b70c87e26f272684ae0c1a2
Christian Sena Gomes <164388478+christiansenagomes@users.noreply.github.com>
2024-12-12 21:58:20 -0300

    Mudança links header2

--- src/front/headerTemplate.js
+++ src/front/headerTemplate.js
@@ -16,9 +16,9 @@ class GreenmindHeader extends HTMLElement {
                         <a href="#">Conheça o<br> Greenmind</a>
                         <div class="dropdown-div">
                             <ul>
-                                <li><a href="#nossaequipe">Nossa equipe</a></li>
-                                <li><a href="#assuntos">Assuntos abordados</a></li>
-                                <li><a href="#greenmind">O Greenmind</a></li>
+                                <li><a href="../../views/nossa-equipe/nossaequipe.html">Nossa equipe</a></li>
+                                <li><a href="../../views/assuntos-abordados/assuntos-abordados.html">Assuntos abordados</a></li>
+                                <li><a href="../../views/o-greenmind/o-greenmind.html">O Greenmind</a></li>
                             </ul>
                         </div>
                     </li>
@@ -26,9 +26,9 @@ class GreenmindHeader extends HTMLElement {
                         <a href="#">Artigos e<br> Dicas</a>
                         <div class="dropdown-div">
                             <ul>
-                                <li><a href="#tema1">Tema 1</a></li>
-                                <li><a href="#tema2">Tema 2</a></li>
-                                <li><a href="#tema3">Tema 3</a></li>
+                                <li><a href="../../views/temas/tema1.html">Tema 1</a></li>
+                                <li><a href="../../views/temas/tema2.html">Tema 2</a></li>
+                                <li><a href="../../views/temas/tema3.html">Tema 3</a></li>
                             </ul>
                         </div>
                     </li>
@@ -36,9 +36,8 @@ class GreenmindHeader extends HTMLElement {
                         <a href="#">Quiz<br> Ambiental</a>
                         <div class="dropdown-div">
                             <ul>
-                                <li><a href="../../views/quiz-pegada/index.html">Pegada Ecológica.</a></li>
-                                <li><a href="#tema2">Tema 2</a></li>
-                                <li><a href="#tema3">Tema 3</a></li>
+                                <li><a href="../../views/formulario/formulario.html">Teste seu Conhecimento</a></li>
+                                <li><a href="../../views/desafio/index.html">Tema 2</a></li>
                             </ul>
                         </div>
                     </li>
@@ -46,13 +45,21 @@ class GreenmindHeader extends HTMLElement {
                         <a href="#">Consumo<br> Inteligente</a>
                         <div class="dropdown-div">
                             <ul>
-                                <li><a href="../../views/pontos-reciclagem/pontos-reciclagem.html">Pontos de Reciclagem</a></li>
-                                <li><a href="#tema2">Tema 2</a></li>
-                                <li><a href="#tema3">Tema 3</a></li>
+                                <li><a href="../../views/desperdicio-agua-energia/desperdicio-agua.html">Consumo de Água</a></li>
+                                <li><a href="../../views/desperdicio-agua-energia/desperdicio-energia.html">Consumo de Luz</a></li>
+                                <li><a href="../../views/desperdicio-alimentos/desperdicio-alimentos.html">Desperdício de alimentos</a></li>
+                                <li><a href="../../views/quiz-pegada/index.html">Pegada Ecológica</a></li>
                             </ul>
                         </div>
                     </li>
-                    <li><a href="#redegreenmind">Rede<br> Greenmind</a></li>
+                    <!--<li class="dropdown">
+                        <a href="#">Rede GreenMind</a>
+                        <div class="dropdown-div">
+                            <ul>
+                                <li><a href="#redegreenmind">Rede<br> Greenmind</a></li>
+                            </ul>
+                        </div>
+                    </li>-->
                     ${this.innerText == "perfil" ? "<li><a href='../../views/perfil/perfil.html'>Perfil</a></li>" : "<li><a href='../../views/auth/login.html'>Login</a></li>"}
                 </ul>
             </nav>
